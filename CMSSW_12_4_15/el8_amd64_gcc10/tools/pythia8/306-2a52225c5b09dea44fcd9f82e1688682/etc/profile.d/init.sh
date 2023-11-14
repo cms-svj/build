2f9fae63ec54650f3b484caa739d7eaf8d1e61a9
Kevin Pedro <kpedro88@gmail.com>
2023-11-14 08:16:04 -0600

    remove old build

--- CMSSW_12_4_15/el8_amd64_gcc10/tools/pythia8/306-2a52225c5b09dea44fcd9f82e1688682/etc/profile.d/init.sh
+++
@@ -1,8 +0,0 @@
-if [ -f /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/el8_amd64_gcc10/tmp/el8_amd64_gcc10/external/pythia8/306-2a52225c5b09dea44fcd9f82e1688682/etc/profile.d/dependencies-setup.sh ]; then . /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/el8_amd64_gcc10/tmp/el8_amd64_gcc10/external/pythia8/306-2a52225c5b09dea44fcd9f82e1688682/etc/profile.d/dependencies-setup.sh; fi
-PYTHIA8_ROOT="/uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/el8_amd64_gcc10/tmp/el8_amd64_gcc10/external/pythia8/306-2a52225c5b09dea44fcd9f82e1688682"
-PYTHIA8_VERSION="306-2a52225c5b09dea44fcd9f82e1688682"
-PYTHIA8_REVISION="1"
-PYTHIA8_CATEGORY="external"
-[ ! -d /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/el8_amd64_gcc10/tmp/el8_amd64_gcc10/external/pythia8/306-2a52225c5b09dea44fcd9f82e1688682/bin ] || export PATH="/uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/el8_amd64_gcc10/tmp/el8_amd64_gcc10/external/pythia8/306-2a52225c5b09dea44fcd9f82e1688682/bin${PATH:+:$PATH}";
-[ ! -d /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/el8_amd64_gcc10/tmp/el8_amd64_gcc10/external/pythia8/306-2a52225c5b09dea44fcd9f82e1688682/lib ] || export LD_LIBRARY_PATH="/uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/el8_amd64_gcc10/tmp/el8_amd64_gcc10/external/pythia8/306-2a52225c5b09dea44fcd9f82e1688682/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}";
-
--- CMSSW_12_4_15/slc7_amd64_gcc10/tools/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3/etc/profile.d/init.sh
+++
@@ -1,8 +0,0 @@
-if [ -f /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/slc7_amd64_gcc10/tmp/slc7_amd64_gcc10/external/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3/etc/profile.d/dependencies-setup.sh ]; then . /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/slc7_amd64_gcc10/tmp/slc7_amd64_gcc10/external/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3/etc/profile.d/dependencies-setup.sh; fi
-EVTGEN_ROOT="/uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/slc7_amd64_gcc10/tmp/slc7_amd64_gcc10/external/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3"
-EVTGEN_VERSION="2.0.0-55fbd35f3034b8518f27ef588686f9a3"
-EVTGEN_REVISION="1"
-EVTGEN_CATEGORY="external"
-[ ! -d /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/slc7_amd64_gcc10/tmp/slc7_amd64_gcc10/external/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3/bin ] || export PATH="/uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/slc7_amd64_gcc10/tmp/slc7_amd64_gcc10/external/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3/bin${PATH:+:$PATH}";
-[ ! -d /uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/slc7_amd64_gcc10/tmp/slc7_amd64_gcc10/external/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3/lib ] || export LD_LIBRARY_PATH="/uscms_data/d3/pedrok/semivisible/production/forRun3/pythia/tmp2/build/CMSSW_12_4_15/slc7_amd64_gcc10/tmp/slc7_amd64_gcc10/external/evtgen/2.0.0-55fbd35f3034b8518f27ef588686f9a3/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}";
-
